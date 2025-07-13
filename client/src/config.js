b7614e80402be0d8dfe59cfa4ad59f10b42f6db0
amin hayouni <hayouniamin2024@gmail.com>
2025-07-13 16:12:35 +0100

    fea

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
 // Use your deployed backend URL here
-const API_BASE_URL = "https://souktek-production.up.railway.app";
+const API_BASE_URL = "www.souktek.shop";
 
 export default API_BASE_URL;
