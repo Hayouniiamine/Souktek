f897347ca68b314eb1bf4332dd38dbe263d61c07
amin hayouni <hayouniamin2024@gmail.com>
2025-07-17 13:03:21 +0100

    f5

--- client/src/components/Bestsellers.jsx
+++ client/src/components/Bestsellers.jsx
@@ -306,7 +306,7 @@ export default function Bestsellers() {
             <div className="text-3xl mb-2">📧</div>
             <h3 className="font-semibold text-lg"> Email delivery</h3>
             <p className="text-gray-400 text-sm mt-1">
-              Your digital Gift card is ready to use within 10 minutes.
+              Your digital Gift card is ready to use within Ten minutes.
             </p>
           </div>
           <div>
